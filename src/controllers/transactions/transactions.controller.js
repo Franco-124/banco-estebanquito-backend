8d93550c0f78484cc3f5755e8114edb21e9374d4
Johan Franco <johan.franco@nousware.ai>
2025-11-03 13:30:05 -0500

    feat: Add new endpoint to verifiy user account number

--- src/controllers/transactions/transactions.controller.js
+++ src/controllers/transactions/transactions.controller.js
@@ -23,10 +23,45 @@ const getTransactionsByUser = async (req, res) => {
     }
 }
 
+const VerifyAccountNumber = async  (req, res) => {
+    try{
+        const connection = await getConnection();
+        const {numero_cuenta} = req.params;
+        console.log(numero_cuenta)
+        const [result] = await connection.query("SELECT * FROM usuarios where numero_cuenta = ?", [numero_cuenta])
+        console.log(result)
+
+        if (result.length > 0){
+            res.json({
+                status_code: 200,
+                success: true,
+                response: "La cuenta del usuario si existe"
+            })
+
+        }else{
+            res.json({
+                status_code: 401,
+                success: false,
+                response: "La cuenta del usuario no pudo ser encontrada"
+            }
+            )
+        }
+
+    }catch(error){
+        res.status(500).json({
+            status_code: 500,
+            success: false,
+            response: "Hubo un error tratanto de verificar el numero de cuenta"
+        })
+    }
+
+}
+
 
 const DepostitMoney = async (req, res) => {
 
 }
+
 
 const WithdrawMoney = async (req, res) => {
     
@@ -41,4 +76,5 @@ const TransferMoney = async (req, res) => {
 
 export const TransactionsMethods  = {
     getTransactionsByUser,
+    VerifyAccountNumber
 }
--- src/routes/transactions/transactions.routes.js
+++ src/routes/transactions/transactions.routes.js
@@ -7,5 +7,8 @@ router.get("/:id",
     TransactionsMethods.getTransactionsByUser
 )
 
+router.get("/cuentas/:numero_cuenta", 
+    TransactionsMethods.VerifyAccountNumber)
+
 
 export default router;
